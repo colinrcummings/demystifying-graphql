bae591e6da5c9a3fe8cd51f9e5322a465c0a525e
Colin Cummings <colin_cummings@brown.edu>
2019-08-20 10:38:29 -0400

    deck

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+const colors = {
+  black: "#202020",
+  gql: "#E10098",
+  white: "white"
+};
+
+const typography = {
+  gql: "'Rubik', sans-serif"
+};
+
+export default {
+  googleFont: "https://fonts.googleapis.com/css?family=Rubik&display=swap",
+  font: typography.gql,
+  colors: {
+    code: colors.gql,
+    background: colors.white,
+    text: colors.black
+  },
+  heading: {
+    color: colors.gql
+  }
+};
